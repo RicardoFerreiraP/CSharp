d530463772e6ceecf10a8dd9c800b4b05a6c5c4d
ricardoferreirap <ricardo.ferreirap@hotmail.com>
2018-05-10 10:32:41 -0300

    Consumindo construtores parametrizados da classe cliente

--- Aulas/Aula1005/Program.cs
+++ Aulas/Aula1005/Program.cs
@@ -4,12 +4,27 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Aula1005
+namespace Aula1005_POO
 {
     class Program
     {
         static void Main(string[] args)
         {
+            Cliente cli = new Cliente();
+            cli.Nome = "Godofredo";  //set
+            string nomeCliente = cli.Nome; //get
+
+            cli.cpf = "12345678909"; //set
+            string cpfCliente = cli.cpf; //get
+
+            Console.WriteLine(cli.Nome);
+            Console.WriteLine(cli.cpf);
+
+            //Utilizando os construtores parametrizados
+            Cliente cli2 = new Cliente("Jucicreia"); //nome
+            Cliente cli3 = new Cliente("Juciliuda", "12312312312"); //nome e cpf
+
+            Console.ReadKey();
         }
     }
 }
