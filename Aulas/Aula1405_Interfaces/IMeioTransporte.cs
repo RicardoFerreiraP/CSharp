921d64811d43bea31e4883a17db7cbd7d0d9e905
Ricardo Ferreira <ricardo.ferreirap@hotmail.com>
2018-05-14 10:59:36 -0300

    Apresentação relacionamento 1 pra 1

--- Aulas/Aula1405_Interfaces/IMeioTransporte.cs
+++ Aulas/Aula1405_Interfaces/IMeioTransporte.cs
@@ -9,6 +9,7 @@ namespace Aula1405_Interfaces
     interface IMeioTransporte
     {
         int Velocidade { get; set; }
+        string Modelo { get; set; }
         void Acelerar();
         void Desacelerar();
         string ImprimirInfo();
