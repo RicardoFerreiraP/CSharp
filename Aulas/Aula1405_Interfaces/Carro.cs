cba360dcc2c9b6fed8d1d0aa37af9c227fc9ada7
Ricardo Ferreira <ricardo.ferreirap@hotmail.com>
2018-05-14 08:53:46 -0300

    Implementando IMeioTransporte

---
+++ Aulas/Aula1405_Interfaces/Carro.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1405_Interfaces
+{
+    class Carro : IMeioTransporte
+    {
+        public int Velocidade { get; set; }
+
+        public Carro()
+        {
+            Velocidade = 0;
+        }
+
+        public void Acelerar()
+        {
+            Velocidade += 10;
+        }
+
+        public void Desacelerar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
