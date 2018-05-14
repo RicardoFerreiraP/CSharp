d8638c33a9eece0bacae9026550f9f5ef467af00
Ricardo Ferreira <ricardo.ferreirap@hotmail.com>
2018-05-14 10:51:20 -0300

    Criado classe Marca e os relacionamentos

---
+++ Aulas/Aula1405_Interfaces/Marca.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1405_Interfaces
+{
+    class Marca
+    {
+        public int MarcaID { get; set; }
+
+        public string Nome { get; set; }
+    }
+}
