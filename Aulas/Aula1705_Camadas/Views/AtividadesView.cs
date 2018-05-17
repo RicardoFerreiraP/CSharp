b5a7255859d867760a6ae3d2f4f8948fd8e65748
Ricardo Ferreira <ricardo.ferreirap@hotmail.com>
2018-05-17 09:24:52 -0300

    Criando classes AtividadesView e criando método Menu

---
+++ Aulas/Aula1705_Camadas/Views/AtividadesView.cs
@@ -0,0 +1,31 @@
+﻿using Aula1705_Camadas.Controllers;
+using Aula1705_Camadas.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1705_Camadas.Views
+{
+    class AtividadesView
+    {
+        public int ExibirMenu()
+        {
+            Console.WriteLine("========================================");
+            Console.WriteLine("=          Escolha uma opção:          =");
+            Console.WriteLine("=         1 - Criar Atividade          =");
+            Console.WriteLine("=         2 - Listar Atividade         =");
+            Console.WriteLine("=         3 - Buscar Atividade         =");
+            Console.WriteLine("=         4 - Editar Atividade         =");
+            Console.WriteLine("=         5 - Excluir Atividade        =");
+            Console.WriteLine("========================================");
+
+            int opcao = int.Parse(Console.ReadLine());
+            return opcao;
+        }
+
+        
+
+    }
+}
