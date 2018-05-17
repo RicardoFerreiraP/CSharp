01428d9bb01f01cb78bff40662893667845202d7
Ricardo Ferreira <ricardo.ferreirap@hotmail.com>
2018-05-17 09:10:39 -0300

    Adicionado métodos na Controller

--- Aulas/Aula1705_Camadas/Controllers/AtividadesController.cs
+++ Aulas/Aula1705_Camadas/Controllers/AtividadesController.cs
@@ -12,12 +12,53 @@ namespace Aula1705_Camadas.Controllers
         //simulando tabela
         public List<Atividade> ListaAtividades { get; set; }
 
-
-
         //Salvar
+        public void Salvar(Atividade atividade)
+        {
+            ListaAtividades.Add(atividade);
+        }
+
         //Listar
-        //Ler
+        public List<Atividade> Listar()
+        {
+            return ListaAtividades;
+        }
+
+        //BuscarPorID
+        public Atividade BuscarPorID(int id)
+        {
+            foreach (Atividade a in ListaAtividades)
+            {
+                if(a.AtividadeID == id)
+                {
+                    return a;
+                }
+            }
+            return null;
+        }
+
         //Editar
+        public void Editar(int id, Atividade atividadeAtualizada)
+        {
+            Atividade atividadeAntiga = BuscarPorID(id);
+
+            if(atividadeAntiga != null)
+            {
+                atividadeAntiga.Nome = atividadeAtualizada.Nome;
+                atividadeAntiga.Ativo = atividadeAtualizada.Ativo;
+            }
+        }
+
         //Excluir
+        public void Excluir(int id)
+        {
+            Atividade atividade = BuscarPorID(id);
+
+            if(atividade != null)
+            {
+                ListaAtividades.Remove(atividade);
+            }
+        }
+
     }
 }
