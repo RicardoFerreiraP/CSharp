28e5049a9ab193fee6d17b2ca73ff1aaa7e3d88f
Ricardo Ferreira <ricardo.ferreirap@hotmail.com>
2018-05-17 08:44:42 -0300

    Criando AtividadesController

---
+++ Aulas/Aula1705_Camadas/Controllers/AtividadesController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1705_Camadas.Controllers
+{
+    class AtividadesController
+    {
+        //simulando tabela
+        public List<Atividade> ListaAtividades { get; set; }
+
+
+
+        //Salvar
+        //Listar
+        //Ler
+        //Editar
+        //Excluir
+    }
+}
