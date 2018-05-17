867b095ef3ecca0a6eb238fe618f4ebeaa9dd0b5
Ricardo Ferreira <ricardo.ferreirap@hotmail.com>
2018-05-17 08:31:39 -0300

    Criado Projeto Camadas, será trabalhado com MVC

---
+++ Aulas/Aula1705_Camadas/Model/Atividade.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Aula1705_Camadas.Model
+{
+    class Atividade
+    {
+        public int AtividadeID { get; set; }
+
+        public string Nome { get; set; }
+
+        public bool Ativo { get; set; }
+
+
+    }
+}
