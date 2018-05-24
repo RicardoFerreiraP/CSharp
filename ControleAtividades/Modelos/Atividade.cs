9e0e05eed2b65ab516ef5f97b0b1960e46b32fb0
Ricardo Ferreira <ricardo.ferreirap@hotmail.com>
2018-05-24 09:19:51 -0300

    Criado classes Usuario, Categoria e Atividade

--- ControleAtividades/Modelos/Atividade.cs
+++ ControleAtividades/Modelos/Atividade.cs
@@ -4,5 +4,16 @@ namespace Modelos
 {
     public class Atividade
     {
+        public int AtividadeID { get; set; }
+
+        public string Nome { get; set; }
+
+        public string Descricao { get; set; }
+
+        public bool Ativo { get; set; }
+
+        public int CategoriaID { get; set; }
+
+        public int UsuarioID { get; set; }
     }
 }
--- ControleAtividades/Modelos/Categoria.cs
+++ ControleAtividades/Modelos/Categoria.cs
@@ -2,5 +2,10 @@
 {
     class Categoria
     {
+        public int CategoriaID { get; set; }
+
+        public string Nome { get; set; }
+
+        public bool Ativo { get; set; }
     }
 }
